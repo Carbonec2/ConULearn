6715dc0103b7bd13ae70de9a5985dbd20d025795
alzha <al_zha@live.concordia.ca>
2017-01-28 17:56:30 -0500

    The registration page has been created. I'm pushing this commit for now to let the front end people do their work.

---
+++ pages/registration.php
@@ -0,0 +1,10 @@
+<div id="mainPanel">
+    <div id="messageBox"></div><br/>
+    <input type="text" class="standardField" id="txtUser" placeholder="Username" style="border-radius:4px;padding:5px; display:inline-block;"/>
+    <br/>
+    <input type="text" class="standardField" id="txtPassword" placeholder="Password" style="border-radius:4px;padding:5px; display:inline-block;"/>
+    <br/>
+       <input type="text" class="standardField" id="txtPassword" placeholder="Password" style="border-radius:4px;padding:5px; display:inline-block;"/>
+    <br/>
+    <input type="button" value="Register" id="btnRegister" style="width:100px; height:50px; display:inline-block; margin:10px;"/>
+</div>
