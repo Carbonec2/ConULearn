cdf84478b399e9cec1bec4f576087cd6882c6975
Helen2hang <he_hang@live.concordia.ca>
2017-01-28 19:10:19 -0500

    Forgot to add the images and head.php in my previous pull request. Here they are.

--- includes/head.php
+++ includes/head.php
@@ -1,1 +1,13 @@
 <!-- Here is the header, in the <head></head> tags, to call css, javascript, etc.-->
+
+
+<!-- THIS IS FOR BOOTSTRAP -->
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- END FOR BOOTSTRAP-->
