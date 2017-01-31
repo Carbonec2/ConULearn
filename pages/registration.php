3e1b9839e581c0a36a7d5f7cfb5d0d1c11c8c7a4
Wei He <hewei196126@gmail.com>
2017-01-31 00:12:02 -0500

    CSS for registration page

--- pages/registration.php
+++ pages/registration.php
@@ -1,3 +1,25 @@
+<style type="text/css">
+#btnSignup{
+    width: 100%;
+    background-color: #66d9ff;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+input.standardField{
+     width: 25%;
+  background-color: #e6ffff;
+  padding: 12px 20px;
+   margin: 8px 0;
+   border: 1px solid #ccc;
+   border-radius: 4px;
+    box-sizing: border-box;
+}
+</style>
+
 <div id="mainPanel" class="container-fluid text-center">
     <div id="messageBox"></div><br/>
     <input type="text" class="standardField" id="txtUser" placeholder="Username" style="border-radius:4px;padding:5px; display:inline-block;"/>
