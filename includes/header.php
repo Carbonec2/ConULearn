a8ebf53594e2d23057df4bee7a19086fd427f037
Jean-Michel Laliberté <jmfreedom@gmail.com>
2017-01-30 22:20:43 -0500

    Hiding the inputs when the connection is done (issue #37 )

--- includes/header.php
+++ includes/header.php
@@ -1,46 +1,44 @@
 <!--Here is the header, before the main-->
 <header>
+    <?php
+    if (!empty($_SESSION['user'])) {
+        echo 'Logged in as <strong>' . $_SESSION['user'] . '</strong>. - <a href="index.php?page=logout">Logout </a>';
+    } else {
+        echo '<ul id="nav_bar">
+                    <li id="nav_bar_logo"><strong>ConULearn</strong></li>
+                    <li><a href="index.php?page=connect">SIGN IN</a></li>
+                    <li><a href="index.php?page=registration">SIGN UP</a></li>
+                    <li><a href="index.php">HOME</a></li>
+                </ul>';
+    }
+    ?>
+    <!--Image of the header-->
+    <div id="headerdiv" class="container-fluid text-center">
+        <h1>LOGO GOES HERE</h1>
+        <img src="img/header.png" alt="" width="60%" height="60%"/>
+
+        <a id="sign_in_button" href="index.php?page=connect">SIGN IN</a>
+        <a id="sign_up_button" href="index.php?page=registration">SIGN UP</a>
         <?php
-		
-        if (!empty($_SESSION['user'])) {
-            echo 'Logged in as <strong>' . $_SESSION['user'] . '</strong>. - <a href="index.php?page=logout">Logout </a>';
+        parse_str($_SERVER["QUERY_STRING"], $query_array);
+        if (empty($query_array)) {
+            echo '</br></br></br></br></br>';
         } else {
-            echo '
-					<ul id="nav_bar">
-					<li id="nav_bar_logo"><strong>ConULearn</strong></li>
-					<li><a href="index.php?page=connect">SIGN IN</a></li>
-					<li><a href="index.php?page=registration">SIGN UP</a></li>
-					<li><a href="index.php">HOME</a></li>
-					</ul>
-				';
+            if ($_GET['page'] == "registration") {
+                echo '
+			<h3> SIGN UP</h3>
+			<span class="glyphicon glyphicon-triangle-bottom"></span>
+			'
+                ;
+            }
+            if ($_GET['page'] == "connect") {
+                echo '
+			<h3> SIGN IN </h3>
+			<span class="glyphicon glyphicon-triangle-bottom"></span>
+			'
+                ;
+            }
         }
         ?>
-		<!--Image of the header-->
-		<div id="headerdiv" class="container-fluid text-center">
-		<h1>LOGO GOES HERE</h1>
-			<img src="img/header.png" alt="" width="60%" height="60%"/>
-			
-			<a id="sign_in_button" href="index.php?page=connect">SIGN IN</a>
-			<a id="sign_up_button" href="index.php?page=registration">SIGN UP</a>
-			<?php 
-			parse_str($_SERVER["QUERY_STRING"],$query_array);
-			if(empty($query_array)){
-			echo '</br></br></br></br></br>';
-			}else {if($_GET['page']=="registration"){
-			echo '
-				<h3> SIGN UP</h3>
-				<span class="glyphicon glyphicon-triangle-bottom"></span>
-				'
-				;
-			}
-			if($_GET['page']=="connect"){
-			echo '
-				<h3> SIGN IN </h3>
-				<span class="glyphicon glyphicon-triangle-bottom"></span>
-				'
-				;
-			}
-			}
-			?>
-		</div>
+    </div>
 </header>
