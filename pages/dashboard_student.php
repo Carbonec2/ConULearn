b6edc841aada1cc4b876f195c8412fd21fe76c92
sophiaquach16 <sophiaquach16@gmail.com>
2017-02-10 17:33:35 -0500

    changed the dashbord feature buttons in the CSS to class instead of ID

--- pages/dashboard_student.php
+++ pages/dashboard_student.php
@@ -1,37 +1,5 @@
-<!--
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css" />
-        <link rel="stylesheet" href="../css/style.css" />
-
-        <div id="dashboard_logo">ConULearn</div>
-        <div id="dashboard_header">Logged in as: <br>Student Name</div>
-
-        <div id="dashboard_features_button">Home</div>
-        <div id="dashboard_features_button">Course Registration</div>
-        <div id="dashboard_features_button">Courses</div>
-        <div id="dashboard_features_button">Discussion</div>
-        <div id="dashboard_features_button">Feedback</div>
-
-        <div id="dashboard_logout_button">Log Out</div>
-
-        <script></script>
- 
-    </head>
-    <body class="dashboard_wallpaper">
-
-    </body>
-</html>
--->
-<!--
-        <div id="dashboard_features_button">Home</div>
-        <div id="dashboard_features_button">Course Registration</div>
-        <div id="dashboard_features_button">Courses</div>
-        <div id="dashboard_features_button">Discussion</div>
-        <div id="dashboard_features_button">Feedback</div>-->
-
-<!--I have commented everything here, since I did re-arrance the organisation o pages, using the header directly. J-M-->
+<div class="dashboard_features_button">Home</div>
+<div class="dashboard_features_button">Course Registration</div>
+<div class="dashboard_features_button">Courses</div>
+<div class="dashboard_features_button">Discussion</div>
+<div class="dashboard_features_button">Feedback</div>-->
