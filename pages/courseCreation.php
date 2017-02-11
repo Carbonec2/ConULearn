bab98bc5c15e05862474b2295db9ea35373daf7f
orelmoisa <orelmoisa@gmail.com>
2017-02-10 22:22:18 -0500

    Course creation div including course name&descriptions and submit button

--- pages/courseCreation.php
+++ pages/courseCreation.php
@@ -1,8 +1,24 @@
-<?php
+<div id="coursecreation" class="container-fluid text-center">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+		<h1>Create a course</h1>
+		
+		<h2>Please fill out this form to complete the creation of a course</h2>
+		
+		<h3>Course Name</h3>
+		
+		<input type="text" class="formText" id="courseName" style="width:150px">
+	
+		<h4>Please give a brief description of the course inlcuding: number of credits, prequisites needed, content of the course and any other meaningful information. </h4>
+		
+		<h3>Course Description</h3>
+		
+		<textarea class="formText" id="courseDescription" style="width:450px;height:250px; resize: none;" >
+		</textarea>
+		
+	    <h5>Click here to submit</h5>
+	
+		<input type="button" value="Submit" id="btnSubmit" class="creationformButton">
 
+
+	
+		</div>
