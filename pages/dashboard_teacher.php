f93a43b1de2119ccbb03ffe98fdeeb5637f4906b
sophiaquach16 <sophiaquach16@gmail.com>
2017-02-11 19:37:28 -0500

    Added features to dashboard_student.php and dashboard_teacher.php

--- pages/dashboard_teacher.php
+++ pages/dashboard_teacher.php
@@ -1,5 +1,15 @@
-<div class="dashboard_features_button">Home</div>
-<div class="dashboard_features_button">Courses</div>
-<div class="dashboard_features_button">Students</div>
-<div class="dashboard_features_button">Discussion</div>
-<div class="dashboard_features_button">Feedback</div>-->
+<head>
+  <title>Patterns</title>
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+
+<body>
+  <div class="dashboard_features_button">Create a Course</div>
+  <div class="dashboard_features_button">Courses</div>
+  <div class="dashboard_course">SOEN 341</div>
+  <div class="dashboard_course_features">Students</div>
+  <div class="dashboard_course_features">Create a Quiz</div>
+  <div class="dashboard_course_features">Quizzes</div>
+  <div class="dashboard_course_features">Feedback + Grades</div>
+  <div class="dashboard_course_features">Discussion</div>
+</body>
