0bd02bc3b4410cc6a054269a3145e2384cd5513c
z-alex <al_zha@live.concordia.ca>
2017-02-12 17:28:18 -0500

    headerdiv should not appear on the dashboard when the user is logged in.

--- includes/header.php
+++ includes/header.php
@@ -5,24 +5,23 @@
         //echo 'Logged in as <strong>' . $_SESSION['user'] . '</strong>.';
         echo '<ul id="nav_bar">
                     <li id="nav_bar_logo"><strong>ConULearn</strong></li>
-                    <li><a href="index.php"><strong>LOGGED IN AS ' . $_SESSION['user'] . '</strong></a></li>
+                    <li><a href="#"><strong>LOGGED IN AS ' . $_SESSION['user'] . '</strong></a></li>
                     <li><a href="index.php">HOME</a></li>';
-        
-        
-        
-        
+
+
+
+
         //Menu for a teacher
         //This if condition shows the good menu according to the rights of the logged user
-        if($_SESSION['Rights_id'] == 1){ //Let's say teacher == 1, student == 2
-        echo '<li><a href="index.php?page=courseCreation">Course creation</a></li>
+        if ($_SESSION['Rights_id'] == 1) { //Let's say teacher == 1, student == 2
+            echo '<li><a href="index.php?page=courseCreation">Course creation</a></li>
               <li><a href="index.php?page=courseManagement">Course management</a></li>
               <li><a href="index.php?page=teacherDiscussion">Discussions</a></li>
               <li><a href="index.php?page=feedback">Feedback</a></li>';
-        
-        }else{
-            if($_SESSION['Rights_id'] == 2){
-        //This will be the menu for a student
-        echo '<li><a href="index.php?page=courseRegistration">Course registration</a></li>
+        } else {
+            if ($_SESSION['Rights_id'] == 2) {
+                //This will be the menu for a student
+                echo '<li><a href="index.php?page=courseRegistration">Course registration</a></li>
               <li><a href="index.php?page=studentCourses">Courses</a></li>
               <li><a href="index.php?page=studentDiscussion">Discussions</a></li>
               <li><a href="index.php?page=feedback">Feedback</a></li>';
@@ -40,39 +39,54 @@
     ?>
 
     <!--Header div-->
-    <div id="headerdiv" class="container-fluid text-center">
-        <img src="img/logo.png" alt="ConULearn" width="25%" height="25%"/></br>
-        <img src="img/header.png" alt="" width="50%" height="50%"/>
+
+    <?php
+    parse_str($_SERVER["QUERY_STRING"], $query_array);
+
+    if (empty($_SESSION['user'])) {
+        echo '<div id="headerdiv" class="container-fluid text-center">';
+        echo '<img src="img/logo.png" alt="ConULearn" width="25%" height="25%"/></br>
+        <img src="img/header.png" alt="" width="50%" height="50%"/>';
+        echo '<a id="sign_in_button" href="index.php?page=connect">SIGN IN</a>';
+        echo '<a id="sign_up_button" href="index.php?page=registration">SIGN UP</a>';
 
 
-
-        <?php
-        if (empty($_SESSION['user'])) {
-            echo '<a id="sign_in_button" href="index.php?page=connect">SIGN IN</a>';
-            echo '<a id="sign_up_button" href="index.php?page=registration">SIGN UP</a>';
-        } else {
-            echo '<a id="logout_button" href="index.php?page=logout">LOGOUT</a>';
-        }
-        parse_str($_SERVER["QUERY_STRING"], $query_array);
+        echo '</div>';
+    } else {
         if (empty($query_array)) {
-            echo '</br></br></br></br></br>';
-        } else {
-            if ($_GET['page'] == "registration") {
-                echo '
-				<h3> SIGN UP</h3>
-				<span class="glyphicon glyphicon-triangle-bottom"></span>
-				'
-                ;
-            }
-            if ($_GET['page'] == "connect") {
-                echo '
-				<h3> SIGN IN </h3>
-				<span class="glyphicon glyphicon-triangle-bottom"></span>
-				'
-                ;
-            }
+            echo '<div id="headerdiv" class="container-fluid text-center">';
+            echo '<img src="img/logo.png" alt="ConULearn" width="25%" height="25%"/></br>
+        <img src="img/header.png" alt="" width="50%" height="50%"/>';
         }
-        ?>
-    </div>
+        echo '<a id="logout_button" href="index.php?page=logout">LOGOUT</a>';
+        if (empty($query_array)) {
+            echo '</div>';
+        }
+    }
+
+
+    /*
+      parse_str($_SERVER["QUERY_STRING"], $query_array);
+      if (empty($query_array)) {
+      echo '</br></br></br></br></br>';
+      } else {
+      if ($_GET['page'] == "registration") {
+      echo '
+      <h3> SIGN UP</h3>
+      <span class="glyphicon glyphicon-triangle-bottom"></span>
+      '
+      ;
+      }
+      if ($_GET['page'] == "connect") {
+      echo '
+      <h3> SIGN IN </h3>
+      <span class="glyphicon glyphicon-triangle-bottom"></span>
+      '
+      ;
+      }
+      }
+     */
+    ?>
+
 </header>
 
