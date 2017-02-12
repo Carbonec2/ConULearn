a8a4dbf6f7a08b8bf1457b59c90ad1b786b15338
sophiaquach16 <sophiaquach16@gmail.com>
2017-02-11 19:49:45 -0500

    Showed quizzes in teacher dashboard navbar

--- pages/dashboard_teacher.php
+++ pages/dashboard_teacher.php
@@ -9,7 +9,9 @@
   <div class="dashboard_course">SOEN 341</div>
   <div class="dashboard_course_features">Students</div>
   <div class="dashboard_course_features">Create a Quiz</div>
-  <div class="dashboard_course_features">Quizzes</div>
+  <div class="dashboard_course_features">Quiz 1</div>
+  <div class="dashboard_course_features">Quiz 2</div>
+  <div class="dashboard_course_features">Quiz 3</div>
   <div class="dashboard_course_features">Feedback + Grades</div>
   <div class="dashboard_course_features">Discussion</div>
 </body>
