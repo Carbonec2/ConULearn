bc6b8f282344bde0989a12135071ccc7e51c1725
Helen2hang <he_hang@live.concordia.ca>
2017-02-13 14:13:43 -0500

    Removed the text-decoration of links and added hover background-color.

--- pages/courseRegistration.php
+++ pages/courseRegistration.php
@@ -5,6 +5,8 @@ include_once('includes/dashboardSideNavBar.php');
     <div class="dashboardcontainer  text-center">
         <div class="container-fluid dashboardDiv  text-center">
         <!-- Insert what you want inside dashboard here-->
+        <h1>Course Registration</h1>
+        Under construction
         </div>
     </div>
 </body>
