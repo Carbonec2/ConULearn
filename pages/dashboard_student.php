db054e10e25f1d44349d45377d23305eca272c18
orelmoisa <orelmoisa@gmail.com>
2017-02-13 10:44:41 -0500

    added link to course registration when clicking "register for a course"

--- pages/dashboard_student.php
+++ pages/dashboard_student.php
@@ -3,7 +3,6 @@ include_once('includes/dashboardSideNavBar.php');
 ?>
 <body class="dashboard">
 
-
     <div class="dashboardcontainer  text-center">
         <h1>Courses</h1>
 		
@@ -15,7 +14,7 @@ include_once('includes/dashboardSideNavBar.php');
 		<a href="" class="dashboard_box_link"><div class="dashboard_course_box">This here is an overlong course code, although it should not be allowed </br><span class="box_course_semester">Winter 2017</span></div></a>
 		-->
 		
-		<a href="" class="dashboard_box_link"><div class="dashboard_add_course_box">Register for a class</br><span class="box_plus_sign">+</span></div></a>
+		<a href="index.php?page=courseRegistration" class="dashboard_box_link"><div class="dashboard_add_course_box">Register for a class</br><span class="box_plus_sign">+</span></div></a>
     </div>
 
 </body>
