565c1e3eeb05e6244192fa332179eace4616b03f
Helen2hang <he_hang@live.concordia.ca>
2017-02-12 20:10:47 -0500

    Added the links to the side bar,fixed the top navbar&added dashboarddiv.

---
+++ includes/dashboardSideNavBar.php
@@ -0,0 +1,12 @@
+<div >
+    </br></br></br>  
+  <a href="index.php?page=dashboard_teacher" class="dashboard_course">Dashboard</a>
+  <a href="index.php?page=courseCreation" class="dashboard_features_button">Create a Course</a>
+  <a href="index.php?page=courseManagement" class="dashboard_features_button">Courses</a>
+  <div class="dashboard_course_features">Students</div>
+  <div class="dashboard_course_features">Create a Quiz</div>
+  <div class="dashboard_course_features">Quizzes</div>
+  <div class="dashboard_course_features">Feedback + Grades</div>
+  <div class="dashboard_course_features">Discussion</div>
+</div>
+
--- pages/dashboard_teacher.php
+++ pages/dashboard_teacher.php
@@ -1,17 +1,9 @@
 <head>
   <title>Patterns</title>
   <link rel="stylesheet" type="text/css" href="../css/style.css">
+  
 </head>
+<?php
+include_once('includes/dashboardSideNavBar.php');
+?>
 
-<body class="dashboard">
-  <div class="dashboard_features_button">Create a Course</div>
-  <div class="dashboard_features_button">Courses</div>
-  <div class="dashboard_course">SOEN 341</div>
-  <div class="dashboard_course_features">Students</div>
-  <div class="dashboard_course_features">Create a Quiz</div>
-  <div class="dashboard_course_features">Quiz 1</div>
-  <div class="dashboard_course_features">Quiz 2</div>
-  <div class="dashboard_course_features">Quiz 3</div>
-  <div class="dashboard_course_features">Feedback + Grades</div>
-  <div class="dashboard_course_features">Discussion</div>
-</body>
