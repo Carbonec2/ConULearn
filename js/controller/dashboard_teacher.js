6cf35d2a6fba55a90f9b3941200e6ea9a2395f1a
z-alex <al_zha@live.concordia.ca>
2017-02-14 03:33:54 -0500

    All the courses that a teacher teaches are listed on his/her dashboard

---
+++ js/controller/dashboard_teacher.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    bind();
+});
+
+function bind() {
+    var identifiers = {User_id: $("#userId").val()};
+    getCourses(identifiers, getCoursesOk, getCoursesFail);
+}
+
+function getCourses(identifiers, successCallback, errorCallback) {
+    
+    $.ajax({
+        type: "POST",
+        url: "database-model.php",
+        data: {DAO: 'course', method: 'getall2', OV: JSON.stringify(identifiers)},
+        async: true,
+        error: function () {
+            //error 500
+        },
+        success: function (object) {
+            var objects = jQuery.parseJSON(object);
+
+
+            console.log(objects);
+            for(var i=0;i<objects.length;i++){
+                $('#courses_container').html($('#courses_container').html()+'<a href="" class="dashboard_box_link"><div class="dashboard_course_box">'+objects[i].name+'</br><span class="box_course_semester">Winter 2017</span></div></a>');
+            }
+  
+            /*
+            if (typeof objects.error.ok === "undefined" || objects.error.ok !== true) {
+                errorCallback();
+            } else {
+                successCallback(objects);
+            }
+            */
+        }
+    });
+}
+
+function getCoursesOk(o) {
+    if(o.Rights_id == 1){
+        window.location = "index.php?page=dashboard_teacher"; //redirects to teacher dashboard
+    }else{
+        if(o.Rights_id == 2){
+            window.location = "index.php?page=dashboard_student"; //redirects to student dashboard
+        }
+    }
+}
+
+function getCoursesFail(callbackObject) {
+
+    console.log(callbackObject);
+
+    //Make more complete message here
+    //$("#messageBox").html("Error while finding courses.");
+}
--- pages/dashboard_teacher.php
+++ pages/dashboard_teacher.php
@@ -2,20 +2,24 @@
 include_once('includes/dashboardSideNavBar.php');
 ?>
 <body class="dashboard">
-   
-	<div class="dashboardcontainer  text-center">
+    <?php
+    echo '<input type="hidden" name="userId" id="userId" value="' . $_SESSION['userId'] . '">';
+    ?>
+
+    <div class="dashboardcontainer  text-center">
 
         <h1>Courses</h1>
-		
-	<!-- random course boxes for testing purposes
-	<a href="" class="dashboard_box_link"><div class="dashboard_course_box">SOEN 133 </br><span class="box_course_semester">Winter 2017</span></div></a>
-	<a href="" class="dashboard_box_link"><div class="dashboard_course_box">COMP 346 </br><span class="box_course_semester">Winter 2017</span></div></a>
-	<a href="" class="dashboard_box_link"><div class="dashboard_course_box">COMP 371 </br><span class="box_course_semester">Winter 2017</span></div></a>
-	<a href="" class="dashboard_box_link"><div class="dashboard_course_box">COMP 353 </br><span class="box_course_semester">Winter 2017</span></div></a>
-	<a href="" class="dashboard_box_link"><div class="dashboard_course_box">This here is an overlong course code, although it should not be allowed </br><span class="box_course_semester">Winter 2017</span></div></a>
-	-->
-		
-		<a href="index.php?page=courseCreation" class="dashboard_box_link"><div class="dashboard_add_course_box">Create a course</br><span class="box_plus_sign">+</span></div></a>
+        <div id="courses_container">
+            <!-- random course boxes for testing purposes
+            <a href="" class="dashboard_box_link"><div class="dashboard_course_box">SOEN 133 </br><span class="box_course_semester">Winter 2017</span></div></a>
+            <a href="" class="dashboard_box_link"><div class="dashboard_course_box">COMP 346 </br><span class="box_course_semester">Winter 2017</span></div></a>
+            <a href="" class="dashboard_box_link"><div class="dashboard_course_box">COMP 371 </br><span class="box_course_semester">Winter 2017</span></div></a>
+            <a href="" class="dashboard_box_link"><div class="dashboard_course_box">COMP 353 </br><span class="box_course_semester">Winter 2017</span></div></a>
+            <a href="" class="dashboard_box_link"><div class="dashboard_course_box">This here is an overlong course code, although it should not be allowed </br><span class="box_course_semester">Winter 2017</span></div></a>
+            -->
+        </div>
+
+        <a href="index.php?page=courseCreation" class="dashboard_box_link"><div class="dashboard_add_course_box">Create a course</br><span class="box_plus_sign">+</span></div></a>
     </div>
 
 </body>
