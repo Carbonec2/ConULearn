bef01e571f4c8ff71869a93ff525ac37952fc836
Helen2hang <he_hang@live.concordia.ca>
2017-02-19 19:30:47 -0500

    created the div of quiz creation.

---
+++ pages/teacherQuizCreation.php
@@ -0,0 +1,11 @@
+<?php
+include_once('includes/dashboardSideNavBar.php');
+?>
+
+<body class="dashboard">
+    <div class="dashboardcontainer">
+        <div class="dashboardDiv container-fluid text-center">
+            Create a Quiz
+            
+        </div>
+    </div>
