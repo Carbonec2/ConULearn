8a178b1dcf6eb04a7bb1a8cfb358fd707662f4fd
z-alex <al_zha@live.concordia.ca>
2017-02-22 19:30:41 -0500

    Travis requires at least 1 test case

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -4,6 +4,9 @@ include_once(dirname(__FILE__).'/../backendIncludeScript.php');
 
 class PHPTest extends PHPUnit_Framework_TestCase {
 
-
+    public function test()
+     {
+         $this->assertEquals('a', 'a');
+     }
 
 }
