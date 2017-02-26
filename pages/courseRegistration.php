1103c9d1875c99e1672cc1ecf96cd6edb22bdd0c
Wei He <hewei26@yahoo.ca>
2017-02-25 20:16:38 -0500

    Change from </br> to <br/>

--- pages/courseRegistration.php
+++ pages/courseRegistration.php
@@ -6,7 +6,7 @@ include_once('includes/dashboardSideNavBar.php');
         <div class="container-fluid dashboardDiv  text-center" id="CourseRegistration">
             <!-- Insert what you want inside dashboard here-->
             <h1>Course Registration</h1>
-            </br>
+            <br/>
             Select a course to register:
             <select id="course_selection">
                 <!-- <option value="volvo">Volvo</option> -->
@@ -15,7 +15,7 @@ include_once('includes/dashboardSideNavBar.php');
             <br/>
             <h3><span id="courseName"></span></h3>
             <div id="courseDescription"></div>
-            </br>
+            <br/>
             <input type="button" value="Submit" id="btnSubmit" class="formsButton" >
             <div id="messageBox"></div>
         </div>
