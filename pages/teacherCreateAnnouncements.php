d643f2d6b0ff0b3ddd303ca14217d446676ba106
Robert <rnguyen350@gmail.com>
2017-03-08 04:25:40 -0500

    announce form buttons text bigger; clear button works (#122)
    Text in announcement form buttons is bolded and set to 20 pixels.

--- pages/teacherCreateAnnouncements.php
+++ pages/teacherCreateAnnouncements.php
@@ -10,9 +10,9 @@ include_once('includes/dashboardSideNavBar.php');
                 <textarea class="announcement" id="announcement"></textarea>
                 <br/>
                 <input type="button" value="Submit" id="send" style="width:100px; 
-                       height:50px; display:inline-block; margin:10px;"/>
-                <br/>
-                <button>Clear</button>
+                       height:50px; display:inline-block; margin:10px; font-size: 20px; font-weight: bold;"/>
+                <input type="reset" value="Clear" style="width:100px; 
+                       height:50px; display:inline-block; margin:10px; font-size: 20px; font-weight: bold;"/>
             </form>
         </div>
     </div>
