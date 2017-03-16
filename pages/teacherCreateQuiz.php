e545524526b6575b1a3808b47c7d44f18604df7f
orelmoisa <orelmoisa@gmail.com>
2017-03-16 13:19:36 -0400

    added class to text input for answers - to put the style in the external file css

--- pages/teacherCreateQuiz.php
+++ pages/teacherCreateQuiz.php
@@ -32,11 +32,11 @@ include_once('includes/dashboardSideNavBar.php');
 <h3> Question 1:</h3> <textarea class="questiontext" id="question1"> </textarea>
 <br/>
 
-a. <input  size="85" id="q1a" type="text"><input value="1a" name="answers1" type="radio"><br/>
-b. <input size="85" id="q1b" type="text"><input value="1b" name="answers1" type="radio"><br/>
-c. <input  size="85" id="q1c" type="text"><input value="1c" name="answers1" type="radio"><br/> 
-d. <input size="85" id="q1d" type="text"><input value="1d" name="answers1" type="radio"><br/>
-e. <input  size="85" id="q1e" type="text"><input value="1e" name="answers1" type="radio"><br/>
+a. <input class="answers" id="q1a" type="text"> <input value="1a" name="answers1" type="radio"><br/>
+b. <input class="answers" id="q1b" type="text"> <input value="1b" name="answers1" type="radio"><br/>
+c. <input class="answers" id="q1c" type="text"> <input value="1c" name="answers1" type="radio"><br/> 
+d. <input class="answers" id="q1d" type="text"> <input value="1d" name="answers1" type="radio"><br/>
+e. <input class="answers" id="q1e" type="text"> <input value="1e" name="answers1" type="radio"><br/>
  </td>
 
   </tr>
@@ -46,11 +46,11 @@ e. <input  size="85" id="q1e" type="text"><input value="1e" name="answers1" type
  <h3> Question 2:</h3> <textarea class="questiontext" id="question2"> </textarea>
 <br/>
 
-a. <input  size="85" id="q2a" type="text"><input value="2a" name="answers2" type="radio"><br/>
-b. <input size="85" id="q2b" type="text"><input value="2b" name="answers2" type="radio"><br/>
-c. <input  size="85" id="q2c" type="text"><input value="2c" name="answers2" type="radio"><br/>
-d. <input size="85" id="q2d" type="text"><input value="2d" name="answers2" type="radio"><br/>
-e. <input  size="85" id="q2e" type="text"><input value="2e" name="answers2" type="radio"><br/>
+a. <input class="answers" id="q2a" type="text"> <input value="2a" name="answers2" type="radio"><br/>
+b. <input class="answers" id="q2b" type="text"> <input value="2b" name="answers2" type="radio"><br/>
+c. <input class="answers" id="q2c" type="text"> <input value="2c" name="answers2" type="radio"><br/>
+d. <input class="answers" id="q2d" type="text"> <input value="2d" name="answers2" type="radio"><br/>
+e. <input class="answers" id="q2e" type="text"> <input value="2e" name="answers2" type="radio"><br/>
 
  </td>
  
@@ -61,11 +61,11 @@ e. <input  size="85" id="q2e" type="text"><input value="2e" name="answers2" type
  <h3> Question 3:</h3> <textarea class="questiontext" id="question3"> </textarea>
 <br/>
 
-a. <input  size="85" id="q3a" type="text"><input value="3a" name="answers3" type="radio"><br/>
-b. <input size="85" id="q3b" type="text"><input value="3b" name="answers3" type="radio"><br/>
-c. <input  size="85" id="q3c" type="text"><input value="3c" name="answers3" type="radio"><br/>
-d. <input size="85" id="q3d" type="text"><input value="3d" name="answers3" type="radio"><br/>
-e. <input  size="85" id="q3e" type="text"><input value="3e" name="answers3" type="radio"><br/>
+a. <input class="answers" id="q3a" type="text"> <input value="3a" name="answers3" type="radio"><br/>
+b. <input class="answers" id="q3b" type="text"> <input value="3b" name="answers3" type="radio"><br/>
+c. <input class="answers" id="q3c" type="text"> <input value="3c" name="answers3" type="radio"><br/>
+d. <input class="answers" id="q3d" type="text"> <input value="3d" name="answers3" type="radio"><br/>
+e. <input class="answers" id="q3e" type="text"> <input value="3e" name="answers3" type="radio"><br/>
 
 </td>
 </tr>
@@ -75,11 +75,11 @@ e. <input  size="85" id="q3e" type="text"><input value="3e" name="answers3" type
 <h3> Question 4:</h3><textarea class="questiontext" id="question4"> </textarea>
 <br/>
 
-a. <input  size="85" id="q4a" type="text"><input value="4a" name="answers4" type="radio"><br/>
-b. <input size="85" id="q4b" type="text"><input value="4b" name="answers4" type="radio"><br/>
-c. <input  size="85" id="q4c" type="text"><input value="4c" name="answers4" type="radio"><br/>
-d. <input size="85" id="q4d" type="text"><input value="4d" name="answers4" type="radio"><br/>
-e. <input  size="85" id="q4e" type="text"><input value="4e" name="answers4" type="radio"><br/>
+a. <input class="answers" id="q4a" type="text"> <input value="4a" name="answers4" type="radio"><br/>
+b. <input class="answers" id="q4b" type="text"> <input value="4b" name="answers4" type="radio"><br/>
+c. <input class="answers" id="q4c" type="text"> <input value="4c" name="answers4" type="radio"><br/>
+d. <input class="answers" id="q4d" type="text"> <input value="4d" name="answers4" type="radio"><br/>
+e. <input class="answers" id="q4e" type="text"> <input value="4e" name="answers4" type="radio"><br/>
 
 </td>
 
@@ -89,11 +89,11 @@ e. <input  size="85" id="q4e" type="text"><input value="4e" name="answers4" type
 <td>
 <h3> Question 5:</h3><textarea class="questiontext" id="question5"> </textarea>
 <br/>
-a. <input  size="85" id="q5a" type="text"><input value="5a" name="answers5" type="radio"><br/>
-b. <input size="85" id="q5b" type="text"><input value="5b" name="answers5" type="radio"><br/>
-c. <input  size="85" id="q5c" type="text"><input value="5c" name="answers5" type="radio"><br/>
-d. <input size="85" id="q5d" type="text"><input value="5d" name="answers5" type="radio"><br/>
-e. <input  size="85" id="q5e" type="text"><input value="5e" name="answers5" type="radio"><br/>
+a. <input class="answers" id="q5a" type="text"> <input value="5a" name="answers5" type="radio"><br/>
+b. <input class="answers" id="q5b" type="text"> <input value="5b" name="answers5" type="radio"><br/>
+c. <input class="answers" id="q5c" type="text"> <input value="5c" name="answers5" type="radio"><br/>
+d. <input class="answers" id="q5d" type="text"> <input value="5d" name="answers5" type="radio"><br/>
+e. <input class="answers" id="q5e" type="text"> <input value="5e" name="answers5" type="radio"><br/>
 </td>
 </tr>
 
@@ -102,11 +102,11 @@ e. <input  size="85" id="q5e" type="text"><input value="5e" name="answers5" type
 <h3> Question 6:</h3><textarea class="questiontext" id="question6"> </textarea>
 <br/>
 
-a. <input  size="85" id="q6a" type="text"><input value="6a" name="answers6" type="radio"><br/>
-b. <input size="85" id="q6b" type="text"><input value="6b" name="answers6" type="radio"><br/>
-c. <input  size="85" id="q6c" type="text"><input value="6c" name="answers6" type="radio"><br/>
-d. <input size="85" id="q6d" type="text"><input value="6d" name="answers6" type="radio"><br/>
-e. <input  size="85" id="q6e" type="text"><input value="6e" name="answers6" type="radio"><br/>
+a. <input class="answers" id="q6a" type="text"> <input value="6a" name="answers6" type="radio"><br/>
+b. <input class="answers" id="q6b" type="text"> <input value="6b" name="answers6" type="radio"><br/>
+c. <input class="answers" id="q6c" type="text"> <input value="6c" name="answers6" type="radio"><br/>
+d. <input class="answers" id="q6d" type="text"> <input value="6d" name="answers6" type="radio"><br/>
+e. <input class="answers" id="q6e" type="text"> <input value="6e" name="answers6" type="radio"><br/>
 </td>
 </tr>
 
@@ -115,11 +115,11 @@ e. <input  size="85" id="q6e" type="text"><input value="6e" name="answers6" type
 <h3> Question 7:</h3><textarea class="questiontext" id="question7"> </textarea>
 <br/>
 
-a. <input  size="85" id="q7a" type="text"><input value="7a" name="answers7" type="radio"><br/>
-b. <input size="85" id="q7b" type="text"><input value="7b" name="answers7" type="radio"><br/>
-c. <input  size="85" id="q7c" type="text"><input value="7c" name="answers7" type="radio"><br/>
-d. <input size="85" id="q7d" type="text"><input value="7d" name="answers7" type="radio"><br/>
-e. <input  size="85" id="q7e" type="text"><input value="7e" name="answers7" type="radio"><br/>
+a. <input class="answers" id="q7a" type="text"> <input value="7a" name="answers7" type="radio"><br/>
+b. <input class="answers" id="q7b" type="text"> <input value="7b" name="answers7" type="radio"><br/>
+c. <input class="answers" id="q7c" type="text"> <input value="7c" name="answers7" type="radio"><br/>
+d. <input class="answers" id="q7d" type="text"> <input value="7d" name="answers7" type="radio"><br/>
+e. <input class="answers" id="q7e" type="text"> <input value="7e" name="answers7" type="radio"><br/>
 </td>
 
 </tr>
@@ -129,11 +129,11 @@ e. <input  size="85" id="q7e" type="text"><input value="7e" name="answers7" type
 <h3> Question 8:</h3><textarea class="questiontext" id="question8"> </textarea>
 <br/>
 
-a. <input  size="85" id="q8a" type="text"><input value="8a" name="answer8" type="radio"><br/>
-b. <input size="85" id="q8b" type="text"><input value="8b" name="answers8" type="radio"><br/>
-c. <input  size="85" id="q8c" type="text"><input value="8c" name="answers8" type="radio"><br/>
-d. <input size="85" id="q8d" type="text"><input value="8d" name="answers8" type="radio"><br/>
-e. <input  size="85" id="q8e" type="text"><input value="8e" name="answers8" type="radio"><br/>
+a. <input class="answers" id="q8a" type="text"> <input value="8a" name="answer8" type="radio"><br/>
+b. <input class="answers" id="q8b" type="text"> <input value="8b" name="answers8" type="radio"><br/>
+c. <input class="answers" id="q8c" type="text"> <input value="8c" name="answers8" type="radio"><br/>
+d. <input class="answers" id="q8d" type="text"> <input value="8d" name="answers8" type="radio"><br/>
+e. <input class="answers" id="q8e" type="text"> <input value="8e" name="answers8" type="radio"><br/>
 </td>
 
 </tr>
@@ -143,11 +143,11 @@ e. <input  size="85" id="q8e" type="text"><input value="8e" name="answers8" type
 <h3> Question 9:</h3><textarea class="questiontext" id="question9"> </textarea>
 <br/>
 
-a. <input  size="85" id="q9a" type="text"><input value="9a" name="answers9" type="radio"><br/>
-b. <input size="85" id="q9b" type="text"><input value="9b" name="answers9" type="radio"><br/>
-c. <input  size="85" id="q9c" type="text"><input value="9c" name="answers9" type="radio"><br/>
-d. <input size="85" id="q9d" type="text"><input value="9d" name="answers9" type="radio"><br/>
-e. <input  size="85" id="q9e" type="text"><input value="9e" name="answers9" type="radio"><br/>
+a. <input class="answers" id="q9a" type="text"> <input value="9a" name="answers9" type="radio"><br/>
+b. <input class="answers" id="q9b" type="text"> <input value="9b" name="answers9" type="radio"><br/>
+c. <input class="answers" id="q9c" type="text"> <input value="9c" name="answers9" type="radio"><br/>
+d. <input class="answers" id="q9d" type="text"> <input value="9d" name="answers9" type="radio"><br/>
+e. <input class="answers" id="q9e" type="text"> <input value="9e" name="answers9" type="radio"><br/>
 </td>
 
 </tr>
@@ -158,11 +158,11 @@ e. <input  size="85" id="q9e" type="text"><input value="9e" name="answers9" type
 <h3> Question 10:</h3><textarea class="questiontext" id="question10"> </textarea>
 <br/>
 
-a. <input  size="85" id="q10a" type="text"><input value="10a" name="answers10" type="radio"><br/>
-b. <input size="85" id="q10b" type="text"><input value="10b" name="answers10" type="radio"><br/>
-c. <input  size="85" id="q10c" type="text"><input value="10c" name="answers10" type="radio"><br/>
-d. <input size="85" id="q10d" type="text"><input value="10d" name="answers10" type="radio"><br/>
-e. <input size="85" id="q10e" type="text"> <input value="10e" name="answers10" type="radio"><br/>
+a. <input class="answers" id="q10a" type="text"> <input value="10a" name="answers10" type="radio"><br/>
+b. <input class="answers" id="q10b" type="text"> <input value="10b" name="answers10" type="radio"><br/>
+c. <input class="answers" id="q10c" type="text"> <input value="10c" name="answers10" type="radio"><br/>
+d. <input class="answers" id="q10d" type="text"> <input value="10d" name="answers10" type="radio"><br/>
+e. <input class="answers" id="q10e" type="text"> <input value="10e" name="answers10" type="radio"><br/>
 </td>
 </tr>
 
@@ -179,3 +179,4 @@ e. <input size="85" id="q10e" type="text"> <input value="10e" name="answers10" t
     </div>
 
 </body>
+
