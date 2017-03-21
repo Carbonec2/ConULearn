abf8ad37e2e41d5860937e4c141fe542d35aea61
Wei He <hewei196126@gmail.com>
2017-03-20 22:35:38 -0400

    Improve the readability of the code in all the php files in page folder

--- pages/teacherAnswerQuestions.php
+++ pages/teacherAnswerQuestions.php
@@ -1,10 +1,13 @@
 <?php
 include_once('includes/dashboardSideNavBar.php');
 ?>
+
 <body class="dashboard">
+
     <div class="dashboardContainer  text-center">
         <div class="container-fluid dashboardDiv  text-center">
             <!--Insert what you want inside dashboard here-->
         </div>
     </div>
+    
 </body>
--- pages/teacherCreateAnnouncements.php
+++ pages/teacherCreateAnnouncements.php
@@ -1,18 +1,20 @@
 <?php
 include_once('includes/dashboardSideNavBar.php');
 ?>
+
 <div id="consoleLoggerContainer" style="z-index:10000;"></div>
-<body class="dashboard">
-    <div class="dashboardContainer">
-        <div class="dashboardDiv container-fluid text-center">
-            <h2>Announcement Form</h2>
-            <h3>Please note that you will be able to edit and/or delete announcements once they have been posted </h3>
-            <form>
-                <textarea class="announcement" id="announcement"></textarea>
-                <br/>
-                <input type="button" value="Submit" id="send" class="formsButton" />
-                <input type="reset" value="Clear" class="formsButton" />
-            </form>
+  <body class="dashboard">
+      <div class="dashboardContainer">
+          <div class="dashboardDiv container-fluid text-center">
+              <h2>Announcement Form</h2>
+              <h3>Please note that you will be able to edit and/or delete announcements once they have been posted </h3>
+
+              <form>
+                  <textarea class="announcement" id="announcement"></textarea>
+                  <br/>
+                  <input type="button" value="Submit" id="send" class="formsButton" />
+                  <input type="reset" value="Clear" class="formsButton" />
+              </form>
         </div>
     </div>
 </body>
