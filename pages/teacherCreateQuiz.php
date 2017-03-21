af494274a6e007814988eeb3ea172d0550b81fd6
Robert <rnguyen350@gmail.com>
2017-03-21 19:24:51 -0400

    Fixed indenting

--- pages/teacherCreateQuiz.php
+++ pages/teacherCreateQuiz.php
@@ -3,20 +3,16 @@ include_once('includes/dashboardSideNavBar.php');
 ?>
 
 <body class="dashboard">
-
-  <div class="dashboardContainer">
-
-        <div class="dashboardDiv container-fluid text-center">
-            <h1><strong>Quiz Creation Form</strong></h1> <br/>
-            <h3>Please fill out the form below to create a quiz. Provide the following: the title of the quiz, the due date, the 10 multiple choice questions, the 5 possible answers for each question, and mark the correct answer.</h2>
-            <br/>
-            <form>
-		      		<div id="quizContainer" ></div>
-		      		<input type="button" value="Submit" id="saveQuiz" class="formsButton" />
-			      	<input type="reset" value="Clear" class="formsButton" />
-            </form>
-        </div>
-
-    </div>
-
+	<div class="dashboardContainer">
+		<div class="dashboardDiv container-fluid text-center">
+			<h1><strong>Quiz Creation Form</strong></h1> <br/>
+			<h3>Please fill out the form below to create a quiz. Provide the following: the title of the quiz, the due date, the 10 multiple choice questions, the 5 possible answers for each question, and mark the correct answer.</h2>
+			<br/>
+			<form>
+				<div id="quizContainer" ></div>
+				<input type="button" value="Submit" id="saveQuiz" class="formsButton" />
+				<input type="reset" value="Clear" class="formsButton" />
+			</form>
+		</div>
+	</div>
 </body>
