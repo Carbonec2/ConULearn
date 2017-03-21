323c9b1357a7aee36690a677a27580d152a14f4b
Wei He <hewei26@yahoo.ca>
2017-03-21 00:11:34 -0400

    Align again the logout.php to make it clear to read

--- pages/logout.php
+++ pages/logout.php
@@ -48,30 +48,30 @@
 			<!--ABOUT US DIV-->
 <div id="aboutUs" class="container-fluid text-center">
 
-		<div class="row">
+	<div class="row">
 			<div class="col-sm-1 text-center">
-		</div>
+			</div>
 
-		<div class="col-sm-10 text-center">
-			<h1>ABOUT US</h1>
-			<h2>Welcome to ConULearn</h2>
-			<h3 class="lighter">We are here to help students of Concordia University get the best learning experience.
-			Our team consists of eight highly qualified Concordia undergraduate students: Wei He, Lunecy Appadoo, Orel Moisa, Jean-Michel Laliberte, Sophia Quach, Alex Zhang, Helen Zhang and Robert Nam Quang Nguyen.
-			Dividing our team into front-end and back-end developers, we are able to create easy to use features.
-			Our main goal is to provide Concordia faculty members with a reliable resource to assess their students progression while enabling them to acquire knowledge through engagements in discussions.
-			</h3>
-			<br/>
-			<h2>How does it work?</h2>
-			<h3 class="lighter">The quiz dates will be announced by the course instructors, after which students will be able to prepare by learning the appropriate material.
-			If any questions come up, the discussion board will be available for use.
-			Professors will then post quizzes that will contain multiple choice questions and short or long answer questions.
-			Once all the students submit their quizzes, it's time for grading!
-			The students will then receive feedback where they will be able to revise their work and learn from their mistakes.</h3>
-			<br/>
-		</div>
+			<div class="col-sm-10 text-center">
+				<h1>ABOUT US</h1>
+				<h2>Welcome to ConULearn</h2>
+				<h3 class="lighter">We are here to help students of Concordia University get the best learning experience.
+				Our team consists of eight highly qualified Concordia undergraduate students: Wei He, Lunecy Appadoo, Orel Moisa, Jean-Michel Laliberte, Sophia Quach, Alex Zhang, Helen Zhang and Robert Nam Quang Nguyen.
+				Dividing our team into front-end and back-end developers, we are able to create easy to use features.
+				Our main goal is to provide Concordia faculty members with a reliable resource to assess their students progression while enabling them to acquire knowledge through engagements in discussions.
+				</h3>
+				<br/>
+				<h2>How does it work?</h2>
+				<h3 class="lighter">The quiz dates will be announced by the course instructors, after which students will be able to prepare by learning the appropriate material.
+				If any questions come up, the discussion board will be available for use.
+				Professors will then post quizzes that will contain multiple choice questions and short or long answer questions.
+				Once all the students submit their quizzes, it's time for grading!
+				The students will then receive feedback where they will be able to revise their work and learn from their mistakes.</h3>
+				<br/>
+			</div>
 
-		<div class="col-sm-1 text-center">
-		</div>
-		
+			<div class="col-sm-1 text-center">
+			</div>
+	</div>	
 </div>
 			<!--END OF ABOUT US-->
