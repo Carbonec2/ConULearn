60f5a8744814a247e95642b573d36c2942b34018
Robert <rnguyen350@gmail.com>
2017-03-22 15:34:41 -0400

    Labelled questions with numbers & answers with letters (#133)
    - Labelled the questions with numbers (Question #1, Question #2, and so on)
    - Labelled the answer options with letters (A, B, C, D, E)

--- js/controller/teacherDisplayQuiz.js
+++ js/controller/teacherDisplayQuiz.js
@@ -50,17 +50,18 @@ function fillQuizQuestions(identifiers) {
             //window.location = "index.php?page=dashboardTeacher";
 
             var objects = jQuery.parseJSON(object);
+            var questionNumber = 1;
 
             console.log(objects);
 
             //$(quizInfo).html(objects.name + " Due date: " + objects.date);
             objects.forEach(function (entry) {
-                $('#questionsContainer').append('<h3 class="quizQuestionsFont">Question: ' + entry.question + '</h3><br/>');
-                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop1' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop1 + '" disabled=disabled> ' + entry.prop1 + '<br/>');
-                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop2' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop2 + '" disabled=disabled> ' + entry.prop2 + '<br/>');
-                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop3' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop3 + '" disabled=disabled> ' + entry.prop3 + '<br/>');
-                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop4' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop4 + '" disabled=disabled> ' + entry.prop4 + '<br/>');
-                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop5' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop5 + '" disabled=disabled> ' + entry.prop5 + '<br/>');
+                $('#questionsContainer').append('<h3 class="quizQuestionsFont">Question #' + (questionNumber++) + ': ' + entry.question + '</h3>');
+                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop1' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop1 + '" disabled=disabled> <strong>A.</strong> ' + entry.prop1 + '<br/>');
+                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop2' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop2 + '" disabled=disabled> <strong>B.</strong> ' + entry.prop2 + '<br/>');
+                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop3' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop3 + '" disabled=disabled> <strong>C.</strong> ' + entry.prop3 + '<br/>');
+                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop4' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop4 + '" disabled=disabled> <strong>D.</strong> ' + entry.prop4 + '<br/>');
+                $('#questionsContainer').append('<input type="radio" id="quiz' + entry.Quiz_id + 'question' + entry.id + 'prop5' + '" class="quizMultipleChoices" name="quiz' + entry.Quiz_id + 'question' + entry.id + '" value="' + entry.prop5 + '" disabled=disabled> <strong>E.</strong> ' + entry.prop5 + '<br/><br/>');
 
                 switch (entry.ans) {
                     case "1":
