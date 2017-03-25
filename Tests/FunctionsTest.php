df00fd7c63643d7c96041d51d7835b56cc9b0a92
z-alex <al_zha@live.concordia.ca>
2017-03-25 18:52:50 -0400

    The quiz creation test has been added to the tests

--- Tests/FunctionsTest.php
+++ Tests/FunctionsTest.php
@@ -2,26 +2,26 @@
 
 include_once(dirname(__FILE__).'/../backendIncludeScript.php');
 
-class PHPTest extends PHPUnit_Framework_TestCase {
+class FunctionsTest extends PHPUnit_Framework_TestCase {
 
-    public function testSignIn() {
+    public function testSignUp() {
         $ov = new stdClass();
         $ov->user = 'user1';
         $ov->password = 'user1';
         $ov->Rights_id = 1;
         
-        $result = UserDAO::insert($ov);
+        $result = UserDAO::insert($ov); //PHP function that is tested
         
         $this->assertTrue($result->error->ok);
     }
     
-    public function testDucplicateUsername() {
+    public function testDucplicateUsernameSignUp() {
         $ov = new stdClass();
         $ov->user = 'user2';
         $ov->password = 'user2';
         $ov->Rights_id = 1;
         
-        UserDAO::insert($ov);
+        UserDAO::insert($ov); //PHP function that is tested
         
         $o = new stdClass();
         $o->user = 'user2';
@@ -43,7 +43,7 @@ class PHPTest extends PHPUnit_Framework_TestCase {
         $ov->description = 'This is a description of the course.';
         
         
-        $result = CourseDAO::insert($ov);
+        $result = CourseDAO::insert($ov); //PHP function that is tested
                 
         $this->assertTrue($result);
     }
@@ -57,7 +57,25 @@ class PHPTest extends PHPUnit_Framework_TestCase {
         $ov->Course_id = '1';
         $ov->description = 'This is an announcement.';
         
-        $result = AnnouncementsDAO::insert($ov);
+        $result = AnnouncementsDAO::insert($ov); //PHP function that is tested
+        
+        $this->assertTrue($result);
+    }
+    
+    public function testQuizCreation() {
+        $ov = new stdClass();
+        
+        $ov->name = 'Quiz 1';
+        $ov->date = '2017-03-20';
+        $ov->Course_id = '1';
+        
+        $object = QuizDAO::insert($ov);
+        
+        if($object->id == '1'){
+            $result = true;
+        }else{
+            $result = false;
+        }
         
         $this->assertTrue($result);
     }
