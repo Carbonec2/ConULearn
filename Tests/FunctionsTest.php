1f7add97fce3d796d6efa9962f91f3491e848b45
z-alex <al_zha@live.concordia.ca>
2017-03-25 19:01:48 -0400

    The quiz question creation test has been added to the tests

--- Tests/FunctionsTest.php
+++ Tests/FunctionsTest.php
@@ -10,7 +10,7 @@ class FunctionsTest extends PHPUnit_Framework_TestCase {
         $ov->password = 'user1';
         $ov->Rights_id = 1;
         
-        $result = UserDAO::insert($ov); //PHP function that is tested
+        $result = UserDAO::insert($ov); //PHP function of our project that is being tested
         
         $this->assertTrue($result->error->ok);
     }
@@ -21,7 +21,7 @@ class FunctionsTest extends PHPUnit_Framework_TestCase {
         $ov->password = 'user2';
         $ov->Rights_id = 1;
         
-        UserDAO::insert($ov); //PHP function that is tested
+        UserDAO::insert($ov); //PHP function of our project that is being tested
         
         $o = new stdClass();
         $o->user = 'user2';
@@ -43,7 +43,7 @@ class FunctionsTest extends PHPUnit_Framework_TestCase {
         $ov->description = 'This is a description of the course.';
         
         
-        $result = CourseDAO::insert($ov); //PHP function that is tested
+        $result = CourseDAO::insert($ov); //PHP function of our project that is being tested
                 
         $this->assertTrue($result);
     }
@@ -57,7 +57,7 @@ class FunctionsTest extends PHPUnit_Framework_TestCase {
         $ov->Course_id = '1';
         $ov->description = 'This is an announcement.';
         
-        $result = AnnouncementsDAO::insert($ov); //PHP function that is tested
+        $result = AnnouncementsDAO::insert($ov); //PHP function of our project that is being tested
         
         $this->assertTrue($result);
     }
@@ -69,13 +69,30 @@ class FunctionsTest extends PHPUnit_Framework_TestCase {
         $ov->date = '2017-03-20';
         $ov->Course_id = '1';
         
-        $object = QuizDAO::insert($ov);
+        $object = QuizDAO::insert($ov); //PHP function of our project that is being tested
         
         if($object->id == '1'){
             $result = true;
         }else{
             $result = false;
         }
+        
+        $this->assertTrue($result);
+    }
+    
+    public function testQuizQuestionCreation() {
+        $ov = new stdClass();
+        
+        $ov->Quiz_id = '1';
+        $ov->question = 'This is a question.';
+        $ov->prop1 = 'This is a proposition 1.';
+        $ov->prop2 = 'This is a proposition 2.';
+        $ov->prop3 = 'This is a proposition 3.';
+        $ov->prop4 = 'This is a proposition 4.';
+        $ov->prop5 = 'This is a proposition 5.';
+        $ov->ans = '3';
+        
+        $result = QuizQuestionDAO::insert($ov); //PHP function of our project that is being tested
         
         $this->assertTrue($result);
     }
