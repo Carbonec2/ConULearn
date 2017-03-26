ca10b0313043db399c63a82bdd1b3b4d98e17869
Helen2hang <Helenee.zhang@hotmail.com>
2017-03-25 20:25:48 -0400

    Removed the fixed number of questions in sentence

--- pages/teacherCreateQuiz.php
+++ pages/teacherCreateQuiz.php
@@ -6,7 +6,7 @@ include_once('includes/dashboardSideNavBar.php');
 	<div class="dashboardContainer">
 		<div class="dashboardDiv container-fluid text-center">
 			<h1><strong>Quiz Creation Form</strong></h1> <br/>
-			<h3>Please fill out the form below to create a quiz. Provide the following: the title of the quiz, the due date, the 10 multiple choice questions, the 5 possible answers for each question, and mark the correct answer.</h3>
+			<h3>Please fill out the form below to create a quiz. Provide the following: the title of the quiz, the due date, the multiple choice questions, the 5 possible answers for each question, and mark the correct answer by selecting the corresponding radio button.  </h3>
 			<br/>
 			<form>
 				<div id="quizContainer" ></div>
