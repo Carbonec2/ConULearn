7b40146f229a60e06b77bce5b05a17fea48aba14
orelmoisa <orelmoisa@gmail.com>
2017-03-26 22:00:43 -0400

    student ask question form

--- pages/studentAskQuestion.php
+++ pages/studentAskQuestion.php
@@ -5,7 +5,21 @@ include_once('includes/dashboardSideNavBar.php');
 <body class="dashboard">
     <div class="dashboardContainer  text-center">
         <div class="container-fluid dashboardDiv  text-center">
-            <!--Insert what you want inside dashboard here-->
+         <h2>Ask your question</h2>
+
+              <h3>The question will be sent to the professor and will be answered as soon as possible. </h3>
+
+              <form>
+
+                  <textarea class="studentQuestion" id="studentQuestion"></textarea>
+
+                  <br/>
+
+                  <input type="button" value="Submit" id="sendQuestion" />
+
+                  <input type="reset" value="Clear" />
+
+              </form>
         </div>
     </div>
 </body>
