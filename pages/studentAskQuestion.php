6875476e0427a2e5f0b89c730fbbec823187d451
orelmoisa <orelmoisa@gmail.com>
2017-03-28 01:13:41 -0400

    Changed title <h2> to <h1>

--- pages/studentAskQuestion.php
+++ pages/studentAskQuestion.php
@@ -5,7 +5,7 @@ include_once('includes/dashboardSideNavBar.php');
 <body class="dashboard">
     <div class="dashboardContainer  text-center">
         <div class="container-fluid dashboardDiv  text-center">
-         <h2>Ask your question</h2>
+            <h1>Ask your question</h1>
 
               <h3>The question will be sent to the professor and will be answered as soon as possible. </h3>
 
